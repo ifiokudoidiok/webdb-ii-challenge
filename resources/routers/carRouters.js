c8382b8516675f0a35c3167f7c132d83ad3e0ff0
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-05 16:50:40 +0100

    Endpoints set up and working!

--- resources/routers/carRouters.js
+++ resources/routers/carRouters.js
@@ -1,19 +1,60 @@
 const express = require('express');
+console.log(process.env.NODE_ENV);
 
-// const db = require('../data/helpers/carModel');
+const db = require('../data/helpers/carModel');
 
 
 const carRouter = express.Router();
 
 carRouter.get('/', getAllCars);
-// carRouter.post('/', addCar);
+carRouter.get('/:id', getCarById);
+carRouter.post('/', addCar);
+carRouter.put('/:id', updateCar);
+carRouter.delete('/:id', deleteCar);
 
-function getAllCars(req,res) {
-    res.send(`<h2>Let's write some middleware!</h2>`)
+
+function deleteCar(req, res) {
+    db.remove(req.params.id)
+    .then(() => {
+        res.status(204)
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'this went wrong: ' + error.message });
+      })
+}
+
+function updateCar(req, res) {
+   //Can be written but skipped on purpose.
+}
+
+function getCarById(req, res) {
+db.findById(req.params.id)
+.then(car => {
+    res.status(200).json(car)
+})
+.catch(error => {
+    res.status(500).json({ message: 'this went wrong: ' + error.message });
+  })
+}
+
+function getAllCars(req, res) {
+    db.find()
+    .then(cars => {
+        res.status(200).json(cars)
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'this went wrong: ' + error.message });
+      })
+    
 }
 
 
-// function addCar(req,res) {
-
-// }
+function addCar(req,res) {
+    db.insert(req.body).then(newCar => {
+        res.status(201).json({
+            success: true,
+            newCar
+        })
+    })
+}
 module.exports = carRouter
